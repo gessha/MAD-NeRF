6cf3dbfa981c087e8e0500dbbffd388eb7bac888
gessha <gosh.georgiev@gmail.com>
2023-03-03 17:24:41 -0500

    comment out TanksTempleDataset

--- dataLoader/__init__.py
+++ dataLoader/__init__.py
@@ -1,14 +1,15 @@
 from .llff import LLFFDataset
 from .blender import BlenderDataset
 from .nsvf import NSVF
-from .tankstemple import TanksTempleDataset, TanksTempleDataset_AUDIO
+# from .tankstemple import TanksTempleDataset, TanksTempleDataset_AUDIO
+from .tankstemple import TanksTempleDataset_AUDIO
 from .your_own_data import YourOwnDataset
 
 
 
 dataset_dict = {'blender': BlenderDataset,
                'llff':LLFFDataset,
-               'tankstemple':TanksTempleDataset,
+            #    'tankstemple':TanksTempleDataset,
                'ad_nerf': TanksTempleDataset_AUDIO,
                'nsvf':NSVF,
                 'own_data':YourOwnDataset}
